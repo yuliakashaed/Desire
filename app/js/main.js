261b8d35cf8270cb4b78311f01735ca191a71d85
yuliakashaed <khatesh@mail.ru>
2021-05-30 16:24:51 +0300

    create blog page

--- app/js/main.js
+++ app/js/main.js
@@ -21,6 +21,15 @@ $(function () {
     arrows: false,
   });
 
+  $('.article-slider__box').slick({
+   prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt="arrow-left"></button>',
+   nextArrow: '<button type="button" class="article-slider__arrow  article-slider__arrowright"><img src="images/arrow-slide-right.svg" alt="arrow-left"></button>',
+
+  });
+
+
+
+
   var mixer = mixitup('.gallery__inner',{
     load: {
       filter: '.living'
